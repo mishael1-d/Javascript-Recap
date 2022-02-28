20801970cf19976a4de09f538176633d34b9204f
Mishael <Mishaeldada@gmail.com>
2022-02-28 15:51:25 +0100

    craeted functions folder

---
+++ Functions/main.js
@@ -0,0 +1,9 @@
+// Get the values for the start and end numbers
+let start = document.getElementById('start').value
+let end = document.getElementById('end').value
+let check = document.getElementById('check')
+
+check.addEventListener('click', function submit (){
+    console.log(start);
+    console.log(end);
+})
---
+++ README.md
@@ -0,0 +1,2 @@
+## JAVASCRIPT TUTORIAL RECAP
+This is me learning the basic concepts of javascript again by building tough projects, or by answering tough challenges.
